2cbfb7b1ab6d2f5a068c93ae6e1a8ba5daa558fa
Nappps <135635413+napppss@users.noreply.github.com>
2025-09-07 10:14:35 +0700

    Create password.js

---
+++ password.js
@@ -0,0 +1,9 @@
+{
+  "secretCode": "specy",
+  "description": "Specy Shop New | BETA", 
+  "lastUpdated": "8/09/2025",
+  "author": "NapsDep", 
+  "status": "Active",
+  "license": "MIT",
+  "version": "6.0"
+    }
